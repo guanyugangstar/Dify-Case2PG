bc3125cc1c9418b24b770820fd3e9c9cbe802e71
guanyugangstar <your@email.com>
2025-07-17 11:16:00 +0800

    feat: 支持PC端摄像头拍照上传，拍摄多张照片可与本地文件一同上传，完善README说明

--- start.bat
+++ start.bat
@@ -11,8 +11,8 @@ REM 2. 激活虚拟环境
 call venv\Scripts\activate.bat
 
 REM 3. 安装依赖
-python -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+REM python -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
+REM pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 REM 4. 用pythonw.exe后台无窗口启动Flask服务
 start /b venv\Scripts\pythonw.exe app.py
